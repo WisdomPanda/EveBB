04c9417ba2fffcaeb66787c27aaa6d71822b963b
WisdomPanda <pandaofwisdom@gmail.com>
2011-04-06 22:47:26 +1000

    First iteration of the auto-patcher.
    Added some missing .html files.

--- update.php
+++ update.php
@@ -8,6 +8,8 @@
 define('PUN_ROOT', './');
 require PUN_ROOT.'include/common.php';
 
+$base_url = 'http://www.eve-bb.com/updates/'.EVE_BB_VERSION.'/';
+
 if ($pun_user['g_id'] != PUN_ADMIN) {
 	message('You must be an admin to do this.');
 } //End if.
@@ -39,71 +41,70 @@ if (!function_exists('fetch_file')) {
 	} //End fetch_file().
 } //End if.
 
-//Lets see if they need to update, using the existing code from admin_index.php, but not using short if format. (Messy IMO.)
-if (!ini_get('allow_url_fopen')) {
-	message('fopen is not enabled. Please fix this before trying again.');
-} //End if.
-
-$latest_version = trim(@file_get_contents('http://www.eve-bb.com/latest_version.txt'));
-
-if (empty($latest_version)) {
-	message('Update check failed.');
-} //End if.
-
-if (version_compare(EVE_BB_VERSION, $latest_version, '>=')) {
-	message('Your EveBB install is up to date!');
-} //End if.
+if (isset($_GET['patch'])) {
+	//The patch file needed a breather, most likely from large SQL queries, we pass it strait back.
+	include(FORUM_CACHE_DIR.EVE_BB_VERSION.'patch.php');
+} else {
 	
-//Ok, from here we want to push these messages to the page right away.
-//This gives the user feed back as the page loads and means they are less likely to do stupid things like hit refresh in the middle of the update.
-//Keep in mind that, while we are telling it to flush the buffer to the browser, this will not always have the correct effect.
-//It varies greatly depending on the browser, server and settings being used. Still, no reason not to use it for those that take advantage of it.
+	//Not in patch, lets run the check.
 
-echo "Newer version found, starting upgrade proccess.<br/>\n";
-ob_fluish();
+	//Lets see if they need to update, using the existing code from admin_index.php, but not using short if format. (Messy IMO.)
+	if (!ini_get('allow_url_fopen')) {
+		message('fopen is not enabled. Please fix this before trying again.');
+	} //End if.
+	
+	$latest_version = trim(@file_get_contents('http://www.eve-bb.com/latest_version.txt'));
+	
+	if (empty($latest_version)) {
+		message('Update check failed.');
+	} //End if.
+	
+	if (version_compare(EVE_BB_VERSION, $latest_version, '>=')) {
+		message('Your EveBB install is up to date!');
+	} //End if.
+		
 
-//Let's get the md5 sum of the updater script, then download the file and check it!
+	echo "Newer version found, starting upgrade proccess.<br/>\n";
+	
+	//Let's get the md5 sum of the updater script, then download the file and check it!
+	echo "Fetching MD5 sum...";
+	
+	if (!fetch_file($base_url.'patch.php.md5', EVE_BB_VERSION.'patch.php.md5')) {
+		echo "<br/>\nUnable to download the checksum for the patch file.<br/>\n";
+		exit;
+	} //End if.
+	
+	echo "Done.<br/>\n";
+	echo "Fetching patch file...";
+	
+	if (!fetch_file($base_url.'patch.php', 'patch.php')) {
+		echo "<br/>\nUnable to download the latest patch.<br/>\n";
+		exit;
+	} //End if.
+	
+	echo "Done.<br/>\n";
+	
+	//Do they match?
+	$md5 = file_get_contents(FORUM_CACHE_DIR.EVE_BB_VERSION.'patch.php.md5');
+	
+	if ($md5 != md5_file(FORUM_CACHE_DIR.'patch.php')) {
+		echo "Patch file does not match the MD5 checksum. Please restart the update.<br/>\n";
+		exit;
+	} //End if.
+	
+	@unlink(FORUM_CACHE_DIR.EVE_BB_VERSION.'patch.php.md5');
+	
+	//Ok, now we hand control over to our patch file. This will download any extra files required and update the DB according to the new scheme.
+	echo "Patch verified, starting patching process.<br/>\n";
 
-echo "Fetching MD5 sum...";
-ob_flush();
+	include(FORUM_CACHE_DIR.'patch.php');
 
-if (!fetch_file('http://www.eve-bb.com/updates/'.EVE_BB_VERSION.'/patch.php.md5', EVE_BB_VERSION.'patch.php.md5')) {
-	echo "<br/>\nUnable to download the checksum for the patch file.<br/>\n";
-	exit;
-} //End if.
+} //End if - else.
 
-echo "Done.<br/>\n";
-ob_flush();
-
-echo "Fetching patch file...";
-ob_flush();
-
-if (!fetch_file('http://www.eve-bb.com/updates/'.EVE_BB_VERSION.'/patch.php', EVE_BB_VERSION.'patch.php')) {
-	echo "<br/>\nUnable to download the latest patch.<br/>\n";
-	exit;
-} //End if.
-
-echo "Done.<br/>\n";
-ob_flush();
-
-//Do they match?
-$md5 = file_get_contents(FORUM_CACHE_DIR.EVE_BB_VERSION.'patch.php.md5');
-
-if ($md5 != md5_file(FORUM_CACHE_DIR.EVE_BB_VERSION.'patch.php')) {
-	echo "Patch file does not match the MD5 checksum. Please restart the update.<br/>\n";
-	exit;
-} //End if.
-
-//Ok, now we hand control over to our patch file. This will download any extra files required and update the DB according to the new scheme.
-echo "Patch verified, starting patching process.<br/>\n";
-ob_flush();
-
-include(FORUM_CACHE_DIR.EVE_BB_VERSION.'patch.php');
-
-if (defined(PATCH_SUCCESS)) {
+if (defined('PATCH_SUCCESS')) {
 	//Lets clean up the cache file now.
 	
-	//TODO: Loop through the file list the patch generates and delete the files.
+	//TODO: Loop through the file list the patch generates and delete the files. Or delete the whole folder + patch file.
 	
 	
 	message('Patch successful! All cache files have been removed and your EveBB install is ready to use.');
